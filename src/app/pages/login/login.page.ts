b447bec87d3369f1efc06bb1bd19ae8c8a4e973b
jmgv87191 <jmgv87191@gmail.com>
2024-04-08 09:14:00 -0700

    ahora si

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
