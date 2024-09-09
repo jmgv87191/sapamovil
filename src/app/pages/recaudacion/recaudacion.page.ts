2235faa4077cc61da95d5d7bbf637418bee7c2c2
juan Manuel <jmgv87191@gmail.com>
2024-09-09 11:01:05 -0700

    asd

--- src/app/pages/recaudacion/recaudacion.page.ts
+++ src/app/pages/recaudacion/recaudacion.page.ts
@@ -28,19 +28,19 @@ export class RecaudacionPage implements OnInit  {
   intersectionObserver: any;
 
   puntos = [ 
-    { lat: 24.1466617, lng: -110.3108386, horaInicio:"13:45", horaFin:"15:45" },
-    { lat: 24.146970, lng: -110.310110 , horaInicio:3, horaFin:7 },
-    { lat: 24.1646662, lng: -110.2993601 , horaInicio:3, horaFin:7 },
-    { lat: 24.1645644, lng: -110.3109223 , horaInicio:3, horaFin:7 },
-    { lat: 24.1388284, lng: -110.2947606 , horaInicio:3, horaFin:7 },
-    { lat: 24.1351511, lng: -110.3255236 , horaInicio:3, horaFin:7 },
-    { lat: 24.1238761, lng: -110.3159922 , horaInicio:3, horaFin:7 },
-    { lat: 24.1180013, lng: -110.3119393 , horaInicio:3, horaFin:7 },
-    { lat: 24.09899, lng: -110.3237345 , horaInicio:3, horaFin:7 },
-    { lat: 24.0621404, lng: -110.3042461 , horaInicio:3, horaFin:7 },
-    { lat: 24.0560987, lng: -110.298443 , horaInicio:3, horaFin:7 },
-    { lat: 24.102710, lng: -110.373431, horaInicio:3, horaFin:7 },
-    { lat: 24.102527, lng: -110.413134, horaInicio:3, horaFin:7 },
+    { lat: 24.1466617, lng: -110.3108386, horaInicio:"09:05", horaFin:"19:40" },
+    { lat: 24.146970, lng: -110.310110 , horaInicio: "08:20" , horaFin:"15:45" },
+    { lat: 24.1646662, lng: -110.2993601 , horaInicio:"08:20", horaFin:"15:45" },
+    { lat: 24.1645644, lng: -110.3109223 , horaInicio:"09:00", horaFin:"15:45" },
+    { lat: 24.1388284, lng: -110.2947606 , horaInicio:"09:00", horaFin:"15:45" },
+    { lat: 24.1351511, lng: -110.3255236 , horaInicio:"09:05", horaFin:"15:45" },
+    { lat: 24.1238761, lng: -110.3159922 , horaInicio:"09:00", horaFin:"15:45" },
+    { lat: 24.1180013, lng: -110.3119393 , horaInicio:"09:00", horaFin:"15:45" },
+    { lat: 24.09899, lng: -110.3237345 , horaInicio:"09:00", horaFin:"15:45" },
+    { lat: 24.0621404, lng: -110.3042461 , horaInicio:"09:00", horaFin:"15:45" },
+    { lat: 24.0560987, lng: -110.298443 , horaInicio:"09:00", horaFin:"15:45" },
+    { lat: 24.102710, lng: -110.373431, horaInicio:"09:00", horaFin:"15:45" },
+    { lat: 24.102527, lng: -110.413134, horaInicio:"09:00", horaFin:"15:45" },
   ]
 
 
@@ -54,12 +54,10 @@ export class RecaudacionPage implements OnInit  {
  */    
     this.loadMap()
     this.addMarker(this.center)
-    setInterval(() => this.checkTimeRange(), 60000); // Verificar cada minuto
-    setInterval(() => {
+
       
       this.checkTimeRange();
-      console.log( this.currentTime )
-    }, 1000);
+
   }
 
   async loadMap(){
@@ -83,47 +81,76 @@ export class RecaudacionPage implements OnInit  {
   }
 
   async addMarker(location: any) {
-
     for (let i = 0; i < this.puntos.length; i++) {
-
-      
       const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
+  
+      // Verifica el rango de tiempo para cada punto
       const markerIcon = document.createElement('img');
-      markerIcon.src = 'assets/new_rojo.jpg';
+      if (this.isInTimeRange(this.puntos[i].horaInicio, this.puntos[i].horaFin)) {
+        markerIcon.src = 'assets/new.png'; // Imagen para el rango válido
+      } else {
+        markerIcon.src = 'assets/new_rojo.jpg'; // Imagen para fuera del rango
+      }
+      
       markerIcon.height = 40;
       markerIcon.width = 40;
   
-  
-      this.marker = new AdvancedMarkerElement({
+      const marker = new AdvancedMarkerElement({
         map: this.map,
         position: this.puntos[i],
         gmpDraggable: false,
         content: markerIcon,
       });
-    }
 
+    // Crea un InfoWindow con los datos
+    const infoWindow = new google.maps.InfoWindow({
+      content: `
+        <div>
+          <h3>Horario</h3>
+          <p> <strong>  Hora Inicio: ${this.puntos[i].horaInicio}</strong></p>
+          <p><strong>Hora Fin: ${this.puntos[i].horaFin}</strong></p>
+        </div>
+      `,
+    });
+
+    // Agrega el listener para abrir el InfoWindow al hacer clic
+    marker.addListener('click', () => {
+      infoWindow.open({
+        anchor: marker,
+        map: this.map,
+        shouldFocus: false,
+      });
+    });
+
+
+
+    }
   }
 
-/*   updateTime(): void {
+  isInTimeRange(startTime: string, endTime: string): boolean {
     const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    this.currentTime = `${hours}:${minutes}`;
-  } */
+    const hours = now.getHours().toString().padStart(2, '0'); // Asegura que siempre tenga dos dígitos
+    const minutes = now.getMinutes().toString().padStart(2, '0'); // Asegura que siempre tenga dos dígitos
+    const currentTime = `${hours}:${minutes}`;
+  
+    return currentTime >= startTime && currentTime <= endTime;
+  }
 
-  checkTimeRange( ): void {
+
+  checkTimeRange(): void {
     const now = new Date();
-    const hours = now.getHours();
-    const minutes = now.getMinutes();
-    this.currentTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}`;
-
-    if ((hours === 14 && minutes >= 8) && (hours === 14 && minutes <= 13) ) {
+    const hours = now.getHours().toString().padStart(2, '0'); // Asegura que siempre tenga dos dígitos
+    const minutes = now.getMinutes().toString().padStart(2, '0'); // Asegura que siempre tenga dos dígitos
+    const currentTime = `${hours}:${minutes}`;
+  
+    const startTime = "0";
+    const endTime = "0";
+  
+    if (currentTime >= startTime && currentTime <= endTime) {
       console.log('cierto');
-      
     } else {
       console.log('falso');
     }
-  }
+  } 
 
 }
